2d99626fef4423f83f05f1cee5378ec714062899
Vijayaraghavan Vashudevan <vijayaraghavan89@gmail.com>
2024-03-14 20:27:33 +0530

    updated

--- setup/init.sh
+++ setup/init.sh
@@ -9,6 +9,8 @@ echo "Downloading tool..."
 curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_linux_amd64 -o aws-iam-authenticator
 chmod +x aws-iam-authenticator
 
+aws eks update-kubeconfig --name cluster --region us-east-1
+
 echo "Updating permissions"
 ./aws-iam-authenticator add user --userarn="${userarn}" --username=github-action-role --groups=system:masters --kubeconfig="$HOME"/.kube/config --prompt=false
 
